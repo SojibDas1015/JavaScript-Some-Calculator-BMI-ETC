ff3eba27edbdca024f360e7827d30f6823973c60
Sojib Das <sojibdas1015@gmail.com>
2025-12-07 21:06:28 +0600

    ternary operatiors

---
+++ turnary.js
@@ -0,0 +1,21 @@
+let Price = 800;
+const leader = false;
+// if(leader == true)
+// {
+//     if(Price > 1000)
+//     {
+//         console.log(Price/2);
+//     }
+//     else
+//     {
+//         console.log(Price=0);
+//     }
+// }
+// else
+// {
+//     console.log(Price);
+// }
+
+// leader === true ? Price > 1000 ? console.log(Price/2) : console.log(Price=0) : console.log(price);
+result = leader === true ? Price > 1000 ? Price/2 : Price=0 : Price;
+console.log(result);
