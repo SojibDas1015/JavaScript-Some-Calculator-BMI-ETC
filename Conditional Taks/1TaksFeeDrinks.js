f16911460ea07fe53f818b99789d4abae5ecbe44
Sojib Das <sojibdas1015@gmail.com>
2025-12-07 22:35:02 +0600

    bmi added condition underweight, normal, overweight

---
+++ Conditional Taks/1TaksFeeDrinks.js
@@ -0,0 +1,9 @@
+let burgerPrice = 600;
+if(burgerPrice > 500)
+{
+    console.log("You Have Free Coke");
+}
+else
+{
+    console.log("Burger + Coke Price: "+(burgerPrice+30));
+}
--- bmi.js
+++ bmi.js
@@ -1,7 +1,24 @@
-const weight = 72;
+const weight = 60;
 const heightfeet = 5;
 const heightinch = 10;
 
 const height = (heightfeet*0.3048) + (heightinch*0.0254);
 const bmi = weight / (height * height);
 console.log(bmi.toFixed(1));
+
+if(bmi < 18.5)
+{
+    console.log("underweight");
+}
+else if(bmi >= 18.5 && bmi <= 24.99)
+{
+    console.log("normal");
+}
+else if(bmi > 25 && bmi <= 29.9)
+{
+    console.log("overweight");
+}
+else
+{
+    console.log("you are obese");
+}
